d9887f9000057d03799724a4afab47f1642861eb
lsaac <2423860442@qq.com>
2023-12-01 16:12:31 +0800

    db封装

--- CaiNiao5_Kotlin_groovy/common/src/main/java/com/echo/common/db/HymDao.kt
+++ CaiNiao5_Kotlin_groovy/common/src/main/java/com/echo/common/db/HymDao.kt
@@ -1,4 +1,57 @@
 package com.echo.common.db
 
-interface HymDao {
+import androidx.room.*
+
+/**
+ * description:demo演示的dao接口文件
+ */
+
+@Dao
+interface UserDao{
+    //查询所有数据，若返回livedata则为 livedata<list<dbuser>>
+    @Query(value = "select * from db_user")
+    fun getAll() : List<DbUser?>?
+
+    @Query(value = "select * from db_user where uid in (:userIds)")
+    fun loadAllByIds(userIds: IntArray?): List<DbUser?>?
+
+    @Query(value = "select * from db_user where uname like :name and age like :age limit 1")
+    fun findByName(name: String?, age: Int):DbUser?
+
+    @Query(value = "select * from db_user where uid like :id")
+    fun getUserById(id:Int):DbUser?
+
+    @Insert
+    fun insertAll(vararg users:DbUser?)//支持可变参数
+
+    @Delete
+    fun delete(user:DbUser) //删除制定的user
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(user: DbUser)//更新，若出现冲突，则使用替换策略，还有其他策略可以选择
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertJUser(user:JUser)
+
+    /**
+     * 查询表tb_juser的数据,返回集合
+     * */
+    @Query("select * from tb_juser")
+    fun queryJuser(): List<JUser>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insertBook(book: Book)
+
+    /**
+     *  表tb_juser插入实体对象JUser
+     * */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertJuser(user: JUser)
+
+    @Query("select * from book")
+    fun getBook():List<Book>
+
+    @Query(value = "select * from tempBean")
+    fun queryUserBook():List<TempBean?>?
+
 }
--- CaiNiao5_Kotlin_groovy/common/src/main/java/com/echo/common/db/JUser.kt
+++ CaiNiao5_Kotlin_groovy/common/src/main/java/com/echo/common/db/JUser.kt
@@ -1,4 +1,27 @@
 package com.echo.common.db
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+
+@Entity(tableName = "tb_juser")
 class JUser {
+    @PrimaryKey(autoGenerate = true)
+    var jId = 0
+    var age = 20
+    var name = "哈哈哈哈"
+
+    private constructor()
+    constructor(age: Int) {
+        this.age = age
+    }
+
+    override fun toString(): String {
+        return "JUser{" +
+                "jId=" + jId +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}'
+    }
 }
